38a143ac78abb1b11592270aeaae65d6ab3b6e0c
FarhadJaan <FarhadJaan501@gmail.com>
2022-11-08 00:24:04 -0800

    First commit

---
+++ src/gitRunner/GitRunner.java
@@ -0,0 +1,18 @@
+package gitRunner;
+
+public class GitRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 20;
+		int b = 30;
+		
+		int m = a +b ;
+		System.out.println(m);
+		
+		
+	}
+
+}
